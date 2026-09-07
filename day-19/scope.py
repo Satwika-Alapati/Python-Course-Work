25d3f87e83bf39a244046792aaddf92420b03284
Satwika-Alapati <satwikaalapati.as@gmail.com>
2026-09-07 16:57:59 +0530

    passbyvr

---
+++ day-19/scope.py
@@ -0,0 +1,56 @@
+'''
+def display(n):
+    n=n+10
+    print('Inside:',n)
+n=10
+display(n)
+print('Outside:',n)
+
+def display():
+    print('Inside:',n)
+
+n=10
+display()
+print('Outside:',n)
+
+def display():
+    n=10
+    print('Inside:',n)
+
+display()
+print('Outside:',n)
+
+
+def display():
+    global n
+    n=n+10
+    print('Inside:',n)
+n=10
+display()
+print('Outside:',n)
+
+
+def display():
+    global n
+    n='PFS'
+    print("Updated Course:",n)
+
+n='JFS'
+display()
+print("Final Course:",n)
+
+def display():
+    n='JFS'
+    def update():
+        nonlocal n
+        n='PFS'
+        print("Updated Course:",n)
+    update()
+    print("Final Course:",n)
+display()
+'''
+
+l=[1,2,3,4,5]
+max=20
+sum=10
+print(sum)
